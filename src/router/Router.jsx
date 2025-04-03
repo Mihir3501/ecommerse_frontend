40da9180c0c4436f965b164668217acabcdccde6
ISHIKAKODHOL <kodholishika0612@gmail.com>
2025-04-03 16:19:40 +0530

    create footer , feedback

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registration from "../user/auth/registration/Registration"; 
 import Login from "../user/auth/login/Login";
 import Mainpage from "../user/pages/mainpage/Mainpage";
-import Admin from "../admin/Admin"
+import Feedback from "../user/pages/feedback/Feedback";
+import Admin_Login from "../admin/pages/Admin_Login"
 import Selar_Registrastion from "../selar/registration/Selar_Registrastion";
 import Selar_Login from "../selar/login/Selar_Login";
 import Selar_Dashboard from"../selar/dashboard/Selar_Dashboard";
@@ -14,17 +15,26 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+
+      {/* User route */} 
         <Route path="/" element={<Mainpage />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/login" element={<Login />} />
         <Route path="/mainpage" element={<Mainpage />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/feedback" element={<Feedback />} />
+     {/* User end */} 
+
+      {/* Admin route */}  
+        <Route path="/admin" element={<Admin_Login />} />
+      {/* Admin end */} 
+
+      {/* Selar route */} 
         <Route path="/selar_Registrastion" element={<Selar_Registrastion/>}/>
         <Route path="/Selar_Login" element={<Selar_Login/>}/>
         <Route path="/Selar_Dashboard" element={<Selar_Dashboard/>}/>
         <Route path="/Selar_Navbar" element={<Selar_Navbar/>}/>
         <Route path="/Selar_Sidebar" element={<Selar_Sidebar/>}/>
-
+      {/* Selar end */}
 
       </Routes>
     </BrowserRouter>
